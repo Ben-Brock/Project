e562d1ee97c461592cfa425f766abc45aba7233b
Ben Brocklebank <ben.brocklebank@gmail.com>
2024-08-13 13:23:45 +0100

    Added base.html, about page, contact page, home page

--- WouldEase/pages/views.py
+++ WouldEase/pages/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse ('WouldEase')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
