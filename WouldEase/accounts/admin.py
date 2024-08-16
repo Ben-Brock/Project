d30bd5f1763263df259668d15c1bd8f8226cbc5a
Ben Brocklebank <ben.brocklebank@gmail.com>
2024-08-16 18:15:31 +0100

    Custom user model added, CSS and bootstrap styling done, one to many mapping created

--- WouldEase/accounts/admin.py
+++ WouldEase/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import Region, Country, CustomUser
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Country)
+admin.site.register(CustomUser, UserAdmin)
+
--- WouldEase/accounts/models.py
+++ WouldEase/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+
+    def is_admin(self):
+        return self.is_admin
+
+class Region(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    code = models.CharField(max_length=2)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.code
