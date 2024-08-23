ef46319f989460d10f21e0a51991981e2637abe8
Ben-Brock <benbrocklebank@gmail.com>
2024-08-23 14:41:22 +0100

    Added thank you page to end of order

--- WouldEase/WouldEase/urls.py
+++ WouldEase/WouldEase/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('search/', include('search_app.urls')),
     path('cart', include('cart.urls')),
     path('shop/',include ('shop.urls')),
+    path('order/',include('order.urls')),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
